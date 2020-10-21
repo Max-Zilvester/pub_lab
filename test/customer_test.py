3ab3192fbe502f176a0115e7c0c75cbae8994198
Max Zilvester <zilimax@gmail.com>
2020-10-21 10:47:52 +0100

    second commit

---
+++ test/customer_test.py
@@ -0,0 +1,5 @@
+  def setUp(self):
+        self.customer = Customer("Joe Bloggs", 10.00)
+
+    # def test_pub_has_name(self):
+    #     self.assertEqual("The Prancing Pony", self.pub.name)
---
+++ test/drink_test.py
@@ -0,0 +1,5 @@
+  def setUp(self):
+        self.drink = Drink("Budweiser", 3.75)
+
+    # def test_pub_has_name(self):
+    #     self.assertEqual("The Prancing Pony", self.pub.name)
